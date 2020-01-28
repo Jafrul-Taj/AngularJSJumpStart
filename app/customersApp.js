ecb49ff7684fc3b5ce63d2cd16adf202a0394518
Jafrul Hasan Taj <jfhasan210@gmail.com>
2020-01-29 00:55:03 +0600

    configuring route

--- app/customersApp.js
+++ app/customersApp.js
@@ -1,5 +1,15 @@
 (function(){
 
-angular.module('customersApp',[]);
+var app = angular.module('customersApp',['ngRoute']);
 
+app.config(function($routeProvider){
+    $routeProvider
+        .when('/',{
+            controller: 'customersController',
+            templateUrl: 'app/Views/customers.html'
+        })
+        .otherwise({redirectTo:'/'});
+
+    });
+    
 }());
